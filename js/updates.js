ef712b9afbcbfadb1c5a286ce9c15d6a9df104bd
Zochg <oscarbp32@gmail.com>
2018-09-07 11:43:08 +0200

    Update updates.js

--- js/updates.js
+++ js/updates.js
@@ -1,1 +1,1 @@
-var news = [{header:"NEW WEBSITE",body:"Hi guys we have a new website and it should work alot better than the old one!"}];
+var news = [{header:"NEW WEBSITE",body:"Hi guys we have a new website and it should work alot better than the old one!"},{header:"test",body:"test"}];
