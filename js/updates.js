f8b8b7b5d8588be6028a8a7e715f017d5bc8c94e
Zochg <oscarbp32@gmail.com>
2018-09-07 11:56:10 +0200

    Add files via upload

--- js/updates.js
+++ js/updates.js
@@ -1,1 +1,1 @@
-var news = [{header:"NEW WEBSITE",body:"Hi guys we have a new website and it should work alot better than the old one!"},{header:"test",body:"test"}];
+var news = [{header:"NEW WEBSITE",body:"Hi guys we have a new website and it should work alot better than the old one!",date:"7/9/18 - 11:52"}];
