c26d17bd3050a5a82b5f4e70b9928942fe0e85a7
Zochg <oscarbp32@gmail.com>
2018-10-01 12:33:36 +0200

    Update updates.js

--- js/updates.js
+++ js/updates.js
@@ -1,11 +1,18 @@
 var news = [
-  {header:"NEW WEBSITE",
+  {
+    header:"New Website",
    body:"Hi guys we have a new website and it should work alot better than the old one!",
-   date:"7/9/18 - 11:52"},
+   date:"7/9/18 - 11:52"
+  },
   {
   header:"Ported games",
     body:"Ported Jar clicker and Evolution cells. Can be found under Games in the sidebar",
     date:"10/9/18 - 8:18"
+  },
+  {
+  header:"New Logo",
+    body:"We created a new logo and we're using on the website now",
+    date:"1/10/18 - 13:32"
   }
 
 ];
